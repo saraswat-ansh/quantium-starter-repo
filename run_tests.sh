e8d51fee06cf6dd767da68b850ea1f0974c88ccf
saraswat-ansh <contactanshsarswat@gmail.com>
2026-03-13 17:35:45 +0530

    Add CI test automation script

---
+++ run_tests.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+source venv/Scripts/activate
+
+pytest
+
+# Check result
+if [ $? -eq 0 ]; then
+    echo "All tests passed"
+    exit 0
+else
+    echo "Tests failed"
+    exit 1
+fi
